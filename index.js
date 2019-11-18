cf66a69a73ad12808e12e0036407679212c8183d
Aisha Rashed <aisha1rashed@gmail.com>
2019-11-18 14:02:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity = "go to the office"){
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(str = "*"){
+  return function(adj = "special"){
+    return `You are ${str}${adj}${str}!`;
+  }
+
+}
+let Calculator={
+  add : function(num1 , num2){
+    return num1+num2;
+  },
+  subtract : function(num1 , num2){
+    return num1-num2;
+  },
+  multiply : function(num1 , num2){
+    return num1*num2;
+  },
+  divide : function(num1 , num2){
+    return num1/num2;
+  }
+}
+
+function actionApplyer(intStart, array ){
+  if (array.length===0)
+  return intStart;
+  
+  else{
+    array.forEach(e=>{
+      intStart = e(intStart);
+    })
+    return intStart;
+  }
+  
+  
+}
+
+let encouragingPromptFunction = wrapAdjective("!!!");
+wrapAdjective("%")("a dedicated programmer") //=> "You are %a dedicated programmer%!"
