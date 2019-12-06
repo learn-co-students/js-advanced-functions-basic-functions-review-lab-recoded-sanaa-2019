7d546d79d7eb7c5d4997760fc13a44877630d670
Sarah Alafif <sar1997yah@gmail.com>
2019-12-06 18:32:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ function saturdayFun(u="roller-skate"){
 function mondayWork(go='go to the office'){
   return `This Monday, I will ${go}.`
 }
-function wrapAdjective
+function wrapAdjective(){
+  
+}
